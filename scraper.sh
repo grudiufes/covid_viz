f968d8e3a0917fff478af8b2bd94d50bbedecf4c
grudiufes <grudi.discurso@gmail.com>
2020-09-07 14:08:24 -0300

    Add automation script

--- scraper.sh
+++ scraper.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if ps -ef | grep -v grep | grep "ttscraper.py corona covid" ; then
-    echo "scraper already running"
-    exit 0
-else
-    echo "scraper not running"
-    python3 ~/Ferramentas/ttscraper.py corona covid &
-    exit 0
+if [[ -z $(ps -ef | grep -v grep | grep "ttscraper.py corona covid") ]]; then
+    xfce4-terminal -e "python3 $HOME/Ferramentas/ttscraper.py corona covid" 
 fi
